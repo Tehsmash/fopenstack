0e3edee5afb0d9f2644c73afc80ea35e370d95af
Sam Betts <sam@code-smash.net>
2014-12-05 12:50:20 +0000

    Collated all fake projects into one package

--- start.sh
+++ start.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-# FIdentity
-pushd ./fidentity 
-
-screen -S fidentity -dm pecan serve config.py
-screen -S fidentityadmin -dm pecan serve configadmin.py
-
-popd
-
-# FCompute
-pushd ./fcompute
-
-screen -S fcompute -dm pecan serve config.py
-
-popd
-
-# FNetworking
-pushd ./fnetworking
-
-screen -S fnetworking -dm pecan serve config.py
-
-popd
-
-# FImage
-pushd ./fimage
-
-screen -S fimage -dm pecan serve config.py
-
-popd
+screen -S fidentity -dm pecan serve config-identity.py
+screen -S fidentityadmin -dm pecan serve config-identityadmin.py
+screen -S fcompute -dm pecan serve config-compute.py
+screen -S fnetworking -dm pecan serve config-networking.py
+screen -S fimage -dm pecan serve config-image.py
